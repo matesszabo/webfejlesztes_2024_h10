1ccf04fd1203f8a030740d50f058e09663fda990
matesszabo <matesszabo@hotmail.com>
2024-11-04 11:16:48 +0100

    mapstruct rendelesmapper list, delete

--- src/main/java/hu/unideb/inf/webshop/service/impl/RendelesAlapAdatServiceImpl.java
+++ src/main/java/hu/unideb/inf/webshop/service/impl/RendelesAlapAdatServiceImpl.java
@@ -1,8 +1,10 @@
 package hu.unideb.inf.webshop.service.impl;
 
+import hu.unideb.inf.webshop.data.entities.RendelesEntity;
 import hu.unideb.inf.webshop.data.repositories.RendelesRepository;
 import hu.unideb.inf.webshop.service.RendelesAlapAdatService;
 import hu.unideb.inf.webshop.service.dto.RendelesDto;
+import hu.unideb.inf.webshop.service.mapper.RendelesMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +15,28 @@ public class RendelesAlapAdatServiceImpl implements RendelesAlapAdatService {
     @Autowired
     RendelesRepository repo;
 
+    @Autowired
+    RendelesMapper mapper;
+
 
     @Override
     public RendelesDto getById(Long id) {
-        return null;
+        //return mapper.rendelesEntityToDto(repo.getReferenceById(id));
+
+        RendelesEntity entity = repo.getReferenceById(id);
+        RendelesDto dto = new RendelesDto();
+
+        dto = mapper.rendelesEntityToDto(entity);
+        return dto;
     }
 
     @Override
     public List<RendelesDto> getAll() {
-        return List.of();
+        return mapper.rendelesEntityListToDtoList(repo.findAll());
     }
 
     @Override
     public void deleteById(Long id) {
-
+        repo.deleteById(id);
     }
 }
