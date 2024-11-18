8757dd786461176f6bed38ec0eba654149792333
matesszabo <matesszabo@hotmail.com>
2024-11-18 11:29:07 +0100

    jwtAuthFilter

---
+++ src/main/java/hu/unideb/inf/webshop/config/JwtAuthFilter.java
@@ -0,0 +1,61 @@
+package hu.unideb.inf.webshop.config;
+
+import hu.unideb.inf.webshop.data.entities.Felhasznalo;
+import hu.unideb.inf.webshop.security.UserDetailsServiceImpl;
+import hu.unideb.inf.webshop.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        //authorization header nem üres és bearer sémát használ
+        if(StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader,"Bearer ")){
+            return ;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        //felhasználónév nem üres, és még nincs bejelentkezve
+        if (StringUtils.isNotEmpty(userEmail)
+                && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails felhasznalo = userDetailsService.loadUserByUsername(userEmail);
+
+            if(jwtService.validateToken(jwt, felhasznalo)){
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        felhasznalo,
+                        null,
+                        felhasznalo.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                context.setAuthentication(authToken);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
